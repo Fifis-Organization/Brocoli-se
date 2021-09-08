76c18170cfb93a5c1d223d653213e914ac131574
SamEngComp <salessamuel268@gmail.com>
2021-09-08 11:28:10 -0300

    files helpers

---
+++ BrocoliSe/BrocoliSe/Protocols/Protocol.swift
@@ -0,0 +1,8 @@
+//
+//  Protocol.swift
+//  BrocoliSe
+//
+//  Created by Samuel Sales on 08/09/21.
+//
+
+import Foundation
---
+++ BrocoliSe/BrocoliSe/View/Fonts/Fonts.swift
@@ -0,0 +1,8 @@
+//
+//  Fonts.swift
+//  BrocoliSe
+//
+//  Created by Samuel Sales on 08/09/21.
+//
+
+import Foundation
