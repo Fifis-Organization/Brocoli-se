28c7dfbb26db7f41370a44518fe56a19ce6de7af
SamEngComp <salessamuel268@gmail.com>
2021-09-13 10:41:50 -0300

    changed name color for blueDark

--- BrocoliSe/BrocoliSe/View/Colors/Colors.swift
+++ BrocoliSe/BrocoliSe/View/Colors/Colors.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static let greenDark        = UIColor(named: "green-dark")
+    static let blueDark        = UIColor(named: "blue-dark")
     static let greenMedium      = UIColor(named: "green-medium")
     static let greenSoft        = UIColor(named: "green-soft")
     static let redCustom        = UIColor(named: "red-custom")
