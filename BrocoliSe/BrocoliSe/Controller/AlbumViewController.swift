3755c297e131c16a2d45929818835ec5ced27809
Nathália Cardoso <crds.nathalia@gmail.com>
2021-09-20 10:14:21 -0300

    setup tela de album

---
+++ BrocoliSe/BrocoliSe/Controller/AlbumViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AlbumViewController.swift
+//  BrocoliSe
+//
+//  Created by Nathalia Cardoso on 20/09/21.
+//
+
+import Foundation
+import UIKit
+
+class AlbumViewController: UIViewController {
+    
+    let scene = AlbumScene(frame: UIScreen.main.bounds)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupSceneConstraints()
+        self.setupNavigationBar()
+    }
+    
+    func setupSceneConstraints() {
+        scene.translatesAutoresizingMaskIntoConstraints = false
+        view.insetsLayoutMarginsFromSafeArea = false
+        view.addSubview(scene)
+        NSLayoutConstraint.activate([
+            scene.topAnchor.constraint(equalTo: view.topAnchor),
+            scene.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scene.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scene.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    
+    func setupNavigationBar() {
+        self.navigationItem.title = "Álbum"
+        
+        let attrs = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont.graviolaRegular(size: 34) ?? UIFont.systemFont(ofSize: 34)
+        ]
+        
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = attrs
+    }
+    
+}
---
+++ BrocoliSe/BrocoliSe/View/Scenes/AlbumScene.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumScene.swift
+//  BrocoliSe
+//
+//  Created by Nathalia Cardoso on 20/09/21.
+//
+
+import UIKit
+import Foundation
+
+class AlbumScene: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.blueDark
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
