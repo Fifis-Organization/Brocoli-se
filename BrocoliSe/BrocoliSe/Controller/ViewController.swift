eba5cf3da26fe0f6ebfb813a3b8ebb75066b5f6c
SamEngComp <salessamuel268@gmail.com>
2021-09-21 09:07:28 -0300

    change setup ViewController and DiaryScene

--- BrocoliSe/BrocoliSe/Controller/ViewController.swift
+++ BrocoliSe/BrocoliSe/Controller/ViewController.swift
@@ -7,24 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    let scene = DiaryScene(frame: UIScreen.main.bounds)
+protocol DiarySceneDelegate: class {
+    func getDayAll(days: [Day])
+    func getFoodAll(foods: [FoodOff])
+    func setController(controller: ViewController)
+    func setupDatas()
+}
 
+class ViewController: UIViewController {
+    private var diaryScene: DiarySceneDelegate?
+    private var coreDataManager: CoreDataManagerProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        scene.translatesAutoresizingMaskIntoConstraints = false
         view.insetsLayoutMarginsFromSafeArea = false
-        view.addSubview(scene)
-        NSLayoutConstraint.activate([
-            scene.topAnchor.constraint(equalTo: view.topAnchor),
-            scene.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scene.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scene.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        navigationController?.navigationBar.isHidden = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+
+    func setDiaryScene(_ aScene: DiarySceneDelegate) {
+        diaryScene = aScene
+        diaryScene?.setController(controller: self)
+        view = diaryScene as? UIView
+        diaryScene?.setupDatas()
+    }
+    
+    func setCoreDataManager(_ aCoreData: CoreDataManagerProtocol) {
+        coreDataManager = aCoreData
+        //testCore()
+    }
+    
+    func fetchFoodAll() {
+        guard let coreDataManager = coreDataManager else { return }
+        let foods: [FoodOff] = coreDataManager.fectch()
+        diaryScene?.getFoodAll(foods: foods)
+    }
+    
+    func testCore() {
+        guard let coreDataManager = coreDataManager else { return }
+        let food01: FoodOff = coreDataManager.createEntity()
+        food01.food = FoodNames.carne
+        let food02: FoodOff = coreDataManager.createEntity()
+        food02.food = FoodNames.ovos
+        let food03: FoodOff = coreDataManager.createEntity()
+        food03.food = FoodNames.frango
+        
+        coreDataManager.saveSync()
     }
 }
