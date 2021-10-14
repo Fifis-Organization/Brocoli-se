a8e430fadca4d04b144842db718d7eb9f28177ab
SamEngComp <salessamuel268@gmail.com>
2021-10-14 10:27:17 -0300

    feature: create mocks

---
+++ BrocoliSe/BrocoliSeTests/Mocks/DiarySceneMock.swift
@@ -0,0 +1,50 @@
+//
+//  DiarySceneMock.swift
+//  BrocoliSeTests
+//
+//  Created by Samuel Sales on 07/10/21.
+//
+
+import Foundation
+import CoreData
+@testable import BrocoliSe
+
+class DiarySceneMock: DiarySceneDelegate {
+    var days: [Day]?
+    var foods: [FoodOff]?
+    var controller: DiaryViewController?
+    var daySelected: Day?
+    var user: User?
+    
+    func setDayAll(days: [Day]) {
+        self.days = days
+    }
+    
+    func setFoodAll(foods: [FoodOff]) {
+        self.foods = foods
+    }
+    
+    func setController(controller: DiaryViewController) {
+        self.controller = controller
+    }
+    
+    func setUser(user: User?) {
+        self.user = user
+    }
+    
+    func setDay(daySelected: Day?) {
+        self.daySelected = daySelected
+    }
+    
+    func presenterModal(_ modal: ModalViewController) {
+        
+    }
+    
+    func setupDatas() {
+        
+    }
+    
+    func setTextLabelProgress(_ text: String) {
+        
+    }
+}
