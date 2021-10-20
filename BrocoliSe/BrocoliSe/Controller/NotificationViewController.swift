f90ff46d067afd879105dcd4e4caa3da72d3d0a2
Paulo Rubem <rubem.oliveira27@hotmail.com>
2021-10-20 08:43:16 -0300

    feature: 61 push notification v5

--- BrocoliSe/BrocoliSe/Controller/NotificationViewController.swift
+++ BrocoliSe/BrocoliSe/Controller/NotificationViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 import UserNotifications
 
+enum MessagesMorning: String, CaseIterable {
+    case morning = "Não vamos perder o foco, cada dia é um avanço importante!"
+}
+enum MessagesNight: String, CaseIterable {
+    case night = "Lembre-se de marcar os item que você não consumiu hoje!"
+}
+
+enum Title: String, CaseIterable {
+    case morning = "Bom dia"
+    case night = "Boa noite"
+}
+
 class NotificationViewController {
     
     func schenduleNotificationMorning(trigger: UNNotificationTrigger) {
@@ -15,8 +27,17 @@ class NotificationViewController {
         let center = UNUserNotificationCenter.current()
               
         let content = UNMutableNotificationContent()
-        content.title = "Bom dia"
-        content.body = "Não vamos perder o foco, cada dia é um avanço importante!"
+        content.title = Title.morning.rawValue
+        content.body = {
+            switch content.title {
+            case Title.morning.rawValue:
+                let text = MessagesMorning.allCases.randomElement()!.rawValue
+                return text
+            default:
+                let text = "Não vamos perder o foco, cada dia é um avanço importante!"
+                return text
+            }
+        }()
         content.sound = .default
 
         let uuidString = UUID().uuidString
@@ -34,8 +55,17 @@ class NotificationViewController {
         let center = UNUserNotificationCenter.current()
               
         let content = UNMutableNotificationContent()
-        content.title = "Boa noite"
-        content.body = "Lembre-se de marcar os item que você não consumiu hoje!"
+        content.title = Title.night.rawValue
+        content.body = {
+            switch content.title {
+            case Title.night.rawValue:
+                let text = MessagesNight.allCases.randomElement()!.rawValue
+                return text
+            default:
+                let text = "Lembre-se de marcar os item que você não consumiu hoje!"
+                return text
+            }
+        }()
         content.sound = .default
 
         let uuidString = UUID().uuidString
