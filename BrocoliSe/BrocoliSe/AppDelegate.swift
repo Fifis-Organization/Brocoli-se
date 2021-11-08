ce678438ba191d9521c71a839054d52d99b34296
Larissa Uchôa <ltuchoa@gmail.com>
2021-11-08 09:17:09 -0300

    fix: remove unused line

--- BrocoliSe/BrocoliSe/AppDelegate.swift
+++ BrocoliSe/BrocoliSe/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // UserDefaults.standard.set(true, forKey: "First Launch")
         if !persistentService.getKeyValue(udKey: .firstLaunch) {
             self.persistentService.persist(udKey: .vibrations, value: true)
             UNUserNotificationCenter.current()
